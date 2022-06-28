91441d9311e8790b9eab30e2a5d9344409c9e7cf
Aryaan202 <86037152+Aryaan202@users.noreply.github.com>
2022-06-28 19:10:51 +0530

    Delete publish.cmd

--- publish.cmd
+++
@@ -1,3 +0,0 @@
-nuget restore
-msbuild QnABot.sln -p:DeployOnBuild=true -p:PublishProfile=.\covid-qna-final-bot-8a1e-Web-Deploy.pubxml -p:Password=QiTqKy3BC7wGvCP3tohJHqpPH4oCpvb4N4b1af5GdnCxiMpXi01l1ZYAxQAd
-
