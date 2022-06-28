f4ce65858558c779c706f9897eb440afc7e2fa38
Aryaan202 <86037152+Aryaan202@users.noreply.github.com>
2022-06-28 14:16:36 +0530

    Add files via upload

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild QnABot.sln -p:DeployOnBuild=true -p:PublishProfile=.\covid-qna-final-bot-8a1e-Web-Deploy.pubxml -p:Password=QiTqKy3BC7wGvCP3tohJHqpPH4oCpvb4N4b1af5GdnCxiMpXi01l1ZYAxQAd
+
